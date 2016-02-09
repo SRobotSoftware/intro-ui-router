ba49643528651f7538caa87821f62bf99fde1962
Zmixon91 <zmixon91@gmail.com>
2016-02-09 10:32:35 -0700

    Chim chim comes back

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,7 +3,11 @@ app.config(function($stateProvider) {
     $stateProvider
     .state('home', {
         url: '/home',
-        template: '<h1>HELLO FROM HOME</h1>'
+        template: '<h1>HELLO FROM HOME</h1> <a href="#/banana">Visit Chim Chim</a>'
+    })
+    .state('view2', {
+        url: '/banana',
+        template: '<h1>HELLO FROM CHIM CHIM</h1><a href="#/home">GO BACK!</a>'
     })
     
 });
